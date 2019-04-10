a2b042c1256a5b38d84a18889c656fa64f40db36
Your Name <you@example.com>
2019-04-10 13:21:33 +0200

    prima parte della simulazione

--- code/js/app.js
+++ code/js/app.js
@@ -7,7 +7,22 @@ var app = {
 	},
 	showCards(dataJson) {
 		console.log(dataJson);
-		
+		dataJson.cardList.forEach(element => {
+			let html =` <div class="col-lg-3 col-md-6 mb-4 net-int">
+			<div class="card h-100">
+			  <img class="card-img-top" src="${element.imgUrl}" alt="CISCO Sim Laptop">
+			  <div class="card-body">
+				<h4 class="card-title">${element.title}</h4>
+				<p class="card-text">${element.desc}</p>
+			  </div>
+			  <div class="card-footer">
+				<a href="${element.buttonUrl}" class="btn btn-primary">Accedi</a>
+			  </div>
+			</div>
+		  </div>
+		</div>`;
+		$("#cardContainer").append(html)
+		});
 	},
 	onError: function(e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
